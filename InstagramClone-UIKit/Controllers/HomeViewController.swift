d38382b29ddc5a5a27dafd16fdce53e0fd6e0677
Erick Harris <erickharris@Ericks-MBP-2.hsd1.ca.comcast.net>
2019-11-22 17:41:54 -0800

    added navbar items to home navbar

--- InstagramClone-UIKit/Controllers/HomeViewController.swift
+++ InstagramClone-UIKit/Controllers/HomeViewController.swift
@@ -20,15 +20,37 @@ class HomeViewController: UIViewController {
     }
     
     private func setupViews() {
+        
+        // MARK: NavBar
+        let cameraButton = UIBarButtonItem(image: UIImage(named: "camera"), style: .plain, target: self, action: #selector(cameraButtonTapped))
+        navigationItem.setLeftBarButton(cameraButton, animated: true)
+        
         logo = UIImageView(image: UIImage(named: "logo"))
         logo.translatesAutoresizingMaskIntoConstraints = false
         navigationItem.titleView = logo
+        
+        let tvButton = UIBarButtonItem(image: UIImage(named: "tv"), style: .plain, target: self, action: #selector(tvButtonTapped))
+        let dmButton = UIBarButtonItem(image: UIImage(named: "dm"), style: .plain, target: self, action: #selector(dmButtonTapped))
+        navigationItem.setRightBarButtonItems([tvButton, dmButton], animated: true)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             logo.widthAnchor.constraint(equalTo: logo.heightAnchor, multiplier: 2.8),
         ])
+    }
+    
+    // MARK: NavBar Selectors
+    @objc func cameraButtonTapped() {
+        
+    }
+    
+    @objc func tvButtonTapped() {
+        
+    }
+    
+    @objc func dmButtonTapped() {
+        
     }
 
 }
